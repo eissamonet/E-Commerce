1d7a279f340535ff2957616ba41959b7aa1cbd0d
Eissa Purnell <eissamonet@gmail.com>
2023-09-03 10:34:32 -0500

    added required info to models

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,6 +1,10 @@
+-- Active: 1692316495732@@127.0.0.1@3306@ecommerce_db
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+
+-- USE DATABASE
+USE ecommerce_db;
 
